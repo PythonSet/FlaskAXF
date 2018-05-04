20a07440cedb970f274884f192f82aa403bfc929
chenzhuo <648346568@qq.com>
2018-05-04 09:46:16 +0800

    轮播图数据插入完毕，

--- App/apis.py
+++ App/apis.py
@@ -1,6 +1,8 @@
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, fields, marshal_with
 
+from App.ext import db
 from App.models import HomeBaner
+from utills.dbutills import add_baner
 
 api = Api()
 
@@ -9,10 +11,28 @@ def init_api(app):
     api.init_app(app)
 
 
+baner_fields = {
+    'id': fields.Integer,
+    'image': fields.String,
+    'name': fields.String,
+    'trackId': fields.String
+}
+
+result_fields = {
+    'msg': fields.String,
+    'baner': fields.List(fields.Nested(baner_fields))
+}
+
+
 class HomeResource(Resource):
+    @marshal_with(result_fields)
     def get(self):
         homebaners = HomeBaner.query.all()
-        return {'msg': 'hello test'}
+        return {'msg': 'hello test', 'baner': homebaners}
+
+    def put(self):
+        add_baner(db)
+        return {'msg': 'ok'}
 
 
 api.add_resource(HomeResource, '/home/')
--- App/models.py
+++ App/models.py
@@ -7,3 +7,6 @@ class HomeBaner(db.Model):
     image = db.Column(db.String(256))
     name = db.Column(db.String(256))
     trackId = db.Column(db.String(32))
+
+    def __repr__(self):
+        return '%s %s %s' % (self.image, self.name, self.trackId)
